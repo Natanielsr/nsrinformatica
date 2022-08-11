ff018c45acca9ec47bbf60cbb299a23692c5b914
Nataniel <contatonatanielsoares@gmail.com>
2022-08-11 14:22:17 -0300

    add composer.json for PHP app detection

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'something/something',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a2bebe0d9feff06777ac358b168b29c6ff892347',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'something/something' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a2bebe0d9feff06777ac358b168b29c6ff892347',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
